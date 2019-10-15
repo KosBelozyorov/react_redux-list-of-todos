182204a761952f93fb4418d81e7f1b303213ae77
KosBelozyorov <KosBelozyorov@gmail.com>
2019-10-15 17:57:25 +0300

    Updates

---
+++ precache-manifest.d7b7b303bcbf582fb3624d962ffa3a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999bbc2438eecdfa5f3627569fd8ed3f",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "3450b20dcef6488dbe93",
+    "url": "/react_redux-list-of-todos/static/css/main.878c4148.chunk.css"
+  },
+  {
+    "revision": "5b7de9cda760b804997f",
+    "url": "/react_redux-list-of-todos/static/js/2.e714e597.chunk.js"
+  },
+  {
+    "revision": "3450b20dcef6488dbe93",
+    "url": "/react_redux-list-of-todos/static/js/main.cbc88001.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
